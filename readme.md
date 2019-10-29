fe9a37970c1fa485585080feee7e7c7b0409df67
nanarino <49119482+nanarino@users.noreply.github.com>
2019-10-29 23:26:05 +0800

    CREATE

---
+++ readme.md
@@ -0,0 +1,53 @@
+# CUR-INF-create
+
+鼠标个性化安装文件`.inf`自动生成脚本
+
+含有两个脚本`CUR_inf_create.py`和`CUR_inf_auto.py`
+
+
+
+- 如果运行`CUR_inf_create.py`会生成`Install.inf`并直接提示：
+
+  Click on the file Install.inf right mouse button, the shortcut menu to choose - to install；
+
+- 如果运行`CUR_inf_auto.py`会生成`Install.inf`生成结束后会询问是否自动安装，
+
+  若按下Y或者直接回车而选择了确认，会直接执行cmd命令来安装inf
+
+
+
+## 注意1
+
+由于执行的安装命令是：`rundll32 syssetup,SetupInfObjectInstallAction DefaultInstall 128 ./Install.inf`可能只在**Windows7**版本可以使用
+
+
+
+## 注意2
+
+使用时必须保证`.cur`或`.ani`有一共**15**个，按文件名称**排序**后需要遵循如下顺序：
+
+1. 正常选择
+2. 帮助选择
+3. 后台运行
+4. 忙
+5. 精确定位
+6. 选定文本
+7. 手写
+8. 不可用
+9. 垂直调整
+10. 水平调整
+11. 沿对角线调整1
+12. 沿对角线调整2
+13. 移动
+14. 候选
+15. 链接选择
+
+
+
+## 注意3
+
+~~DD专用一键导入~~
+
+本脚本使用虚拟Youtuber【宝鐘マリン】的鼠标光标 来作为测试，
+
+使用时请遵循`宝鐘マリンマウスカーソル/readme(はじめにお読みください).md`文件上声明
---
+++ 宝鐘マリンマウスカーソル/CUR_inf_auto.py
@@ -0,0 +1,79 @@
+import os
+import re
+
+CUR_DIR = __file__.split(os.sep)[-2]
+
+def get_zip_file(input_path:str, result:list):
+    files = os.listdir(input_path)
+    for file in files:
+        if os.path.isdir(file):
+            #result.append(file)
+            #get_zip_file(file, result)
+            pass
+        else:
+            result.append(file)
+ls = []
+get_zip_file(os.getcwd(),ls)
+ls = list(filter(lambda name:bool(re.findall(r'(^.*\.ani$)|(^.*\.cur$)', name)),ls))[:15]
+ls.sort()
+
+Strings_list = ['pointer','help','work','busy','cross','text','hand','unavailiable','vert','horz','dgn1','dgn2','move','alternate','link']
+Strings_list = list(map(lambda h,f: h + ' = "' + f + '"\n', Strings_list,ls))
+
+inf_head = r'''[Version]
+signature="$CHICAGO$"
+
+[DefaultInstall]
+CopyFiles = Scheme.Cur
+AddReg    = Scheme.Reg,Wreg
+
+[DestinationDirs]
+Scheme.Cur = 10,"%CUR_DIR%"
+
+[Scheme.Reg]
+HKCU,"Control Panel\Cursors\Schemes","%SCHEME_NAME%",,"%10%\%CUR_DIR%\%pointer%,%10%\%CUR_DIR%\%help%,%10%\%CUR_DIR%\%work%,%10%\%CUR_DIR%\%busy%,%10%\%CUR_DIR%\%Cross%,%10%\%CUR_DIR%\%Text%,%10%\%CUR_DIR%\%Hand%,%10%\%CUR_DIR%\%Unavailiable%,%10%\%CUR_DIR%\%Vert%,%10%\%CUR_DIR%\%Horz%,%10%\%CUR_DIR%\%Dgn1%,%10%\%CUR_DIR%\%Dgn2%,%10%\%CUR_DIR%\%move%,%10%\%CUR_DIR%\%alternate%,%10%\%CUR_DIR%\%link%"
+
+[Wreg]
+HKCU,"Control Panel\Cursors",,0x00020000,"%SCHEME_NAME%"
+HKCU,"Control Panel\Cursors",AppStarting,0x00020000,"%10%\%CUR_DIR%\%work%"
+HKCU,"Control Panel\Cursors",Arrow,0x00020000,"%10%\%CUR_DIR%\%pointer%"
+HKCU,"Control Panel\Cursors",Crosshair,0x00020000,"%10%\%CUR_DIR%\%Cross%"
+HKCU,"Control Panel\Cursors",Hand,0x00020000,"%10%\%CUR_DIR%\%link%"
+HKCU,"Control Panel\Cursors",Help,0x00020000,"%10%\%CUR_DIR%\%Help%"
+HKCU,"Control Panel\Cursors",IBeam,0x00020000,"%10%\%CUR_DIR%\%Text%"
+HKCU,"Control Panel\Cursors",No,0x00020000,"%10%\%CUR_DIR%\%Unavailiable%"
+HKCU,"Control Panel\Cursors",NWPen,0x00020000,"%10%\%CUR_DIR%\%Hand%"
+HKCU,"Control Panel\Cursors",SizeAll,0x00020000,"%10%\%CUR_DIR%\%move%"
+HKCU,"Control Panel\Cursors",SizeNESW,0x00020000,"%10%\%CUR_DIR%\%Dgn2%"
+HKCU,"Control Panel\Cursors",SizeNS,0x00020000,"%10%\%CUR_DIR%\%Vert%"
+HKCU,"Control Panel\Cursors",SizeNWSE,0x00020000,"%10%\%CUR_DIR%\%Dgn1%"
+HKCU,"Control Panel\Cursors",SizeWE,0x00020000,"%10%\%CUR_DIR%\%Horz%"
+HKCU,"Control Panel\Cursors",UpArrow,0x00020000,"%10%\%CUR_DIR%\%alternate%"
+HKCU,"Control Panel\Cursors",Wait,0x00020000,"%10%\%CUR_DIR%\%busy%"
+HKLM,"SOFTWARE\Microsoft\Windows\CurrentVersion\Runonce\Setup\","",,"rundll32.exe shell32.dll,Control_RunDLL main.cpl @0,1"
+
+'''
+
+with open('Install.inf','w') as inf:
+    inf.write(inf_head)
+    inf.write('[Scheme.Cur]\n')
+    for i in ls:
+        inf.write('"' + i + '"\n')
+    inf.write('\n')
+    inf.write('[Strings]\n')
+    inf.write('CUR_DIR = "Cursors\\' + CUR_DIR + '"\n')
+    inf.write('SCHEME_NAME = "' + CUR_DIR + '"\n')
+    inf.write('SCHEME_DESCRIPTION = "' + CUR_DIR + '"\n')
+    inf.writelines(Strings_list)
+
+
+
+i = input("Do you want to install it now?")
+if(i == "")or(i == "Y"):
+    #auto
+    import subprocess
+    s = subprocess.Popen("rundll32 syssetup,SetupInfObjectInstallAction DefaultInstall 128 ./Install.inf", shell=True)
+    s.wait()
+    input("Install Success")
+else:
+    input("Click on the file Install.inf right mouse button, the shortcut menu to choose - to install")
